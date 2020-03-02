f62ddf9caf55bdac9c3331c8eff3dd28ec5bc7fc
Erik Gúth <guthember@gmail.com>
2020-03-02 16:34:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Naprendszer
+{
+  struct Bolygo
+  {
+    public string nev;
+    public int fk;
+    public int ft;
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Adatok eltárolása
+      
+
+      StreamReader file = new StreamReader("bolygok.txt");
+
+      #region Amikor tudjuk, hogy mennyi adat van!
+      //Bolygo[] bolygok = new Bolygo[7];
+      //for (int i = 0; i < 7; i++)
+      //{
+      //  // egy sor beolvasás a fájlból
+      //  string sor = file.ReadLine();
+
+      //  // a "sor" felbontása szóközök mentén
+      //  string[] a = sor.Split(' ');
+
+      //  // adatok átadása
+      //  bolygok[i].nev = a[0];
+      //  bolygok[i].fk = Convert.ToInt32(a[1]);
+      //  bolygok[i].ft = Convert.ToInt32(a[2]);
+      //} 
+      #endregion
+
+      #region Amikor nem tudjuk, hogy mennyi adat van!
+      // max 200 lehet!
+      Bolygo[] bolygok = new Bolygo[200];
+      int db = 0; // tényleges darabszám
+
+      while (!file.EndOfStream)
+      {
+        // egy sor beolvasás a fájlból
+        string sor = file.ReadLine();
+
+        // a "sor" felbontása szóközök mentén
+        string[] a = sor.Split(' ');
+
+        // adatok átadása
+        bolygok[db].nev = a[0];
+        bolygok[db].fk = Convert.ToInt32(a[1]);
+        bolygok[db].ft = Convert.ToInt32(a[2]);
+        db++;
+      }
+
+      #endregion
+
+      file.Close();
+      
+      // Adatok képernyőre írása
+      Console.WriteLine("Bolygók adatai");
+      Console.WriteLine("Név        Földközeli Földtávoli");
+      Console.WriteLine("--------------------------------");
+      // for(int i = 0; i < 7; i++)
+      for (int i = 0; i < db; i++)
+      {
+        Console.WriteLine("{0,-10} {1,10} {2,10}",
+          //bolygok[i].nev.PadRight(10),
+          //bolygok[i].fk.ToString().PadLeft(10),
+          //bolygok[i].ft.ToString().PadLeft(10)
+          bolygok[i].nev,
+          bolygok[i].fk,
+          bolygok[i].ft
+        );
+      }
+      Console.WriteLine("--------------------------------");
+
+
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+﻿# Naprendszer bolygói
+
+## File szerkezete
+	- sorokban az egyes bolygók adatai
+	- név, földközeli távolság, földtávoli távolság
+	- 7 db bolygó adatát tartalmazza
+	- "bolygok.txt"
+
+## Feladat
+	[x] Megfelelő struktúra elkészítése
+	[x] Adatok beolvasása és eltárolása
+	[x] Adatok kiírása
+
+### Megfelelő struktrúra elkészítése
+	- 3 adattagja lesz
+	- string, int, int
+	- neve: Bolygo
+	- adattagok:
+		- nev
+		- fk
+		- ft
+
+### Adatok beolvasása és eltárolása
+	- using System.IO;
+	- tudjuk, hogy 7 db adat van!
+	- Bolygo[] bolygok = new Bolygo[7] (7 elemű tömb, Bolygo típusú elemeket tartalmaz)
+	- StreamReader file = new StreamReader("bolygok.txt")
+	- !!!! használat után file.Close() !!!!
+	- Ciklus az adatok beolvasására
+	- soronként beolvasni
+	- sorokat adatokra bontani
+	- megfelelő adattagonak -> megfelelő értékeket átadni
+
+### Adatok kiírása (képernyőre)
+	- Ciklus
+	- Kultúrált kiírás
+
+
+## Ha nem tudjuk, hogy mennyi adat van a fájlban
+	- Azt tudjuk, hogy max. mennyi lehet, pl.: 100 db
+	- Bolygo[] bolygok = new Bolygo[100] 
+	- mindig annyit használunk -> amennyit beolvasunk
